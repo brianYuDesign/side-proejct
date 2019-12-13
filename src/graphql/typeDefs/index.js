cc8e942abf694f39a99514e5f99f97c2b8f398dc
brianYuDesign <brian.yu@4idps.com>
2019-12-13 22:18:03 +0800

    Config Apollo Server Structure

---
+++ src/graphql/typeDefs/index.js
@@ -0,0 +1,4 @@
+import { typeDefs } from "graphql-scalars"
+import Root from "./root"
+
+export default [...typeDefs, Root]
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from "apollo-server-express"
+import http from "http"
+import express from "express"
+import env from "dotenv"
+import typeDefs from "./graphql/typeDefs"
+import resolvers from "./graphql/resolvers"
+
+env.config()
+
+const { NODE_ENV, PORT } = process.env
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    const accessToken = req.headers["access-token"]
+    if (accessToken) {
+      //return { user: AuthService.getUserFromAccessToken()}
+    }
+    return undefined
+  }
+})
+
+const app = express()
+
+server.applyMiddleware({ app })
+
+const httpServer = http.createServer(app)
+server.installSubscriptionHandlers(httpServer)
+
+httpServer.listen(PORT, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  )
+})
